ae15ace071621cf0ec300ca50f94ac36f20d9917
ina <bbangina@gmail.com>
2020-11-03 18:39:19 +0900

    ADD todoform

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { TodoList } from "./TodoList";
+import { AddTodoForm } from "./AddTodoForm";
 
 const initialTodos: Array<Todo> = [
   { text: "Walk the dog", complete: true },
@@ -9,7 +10,7 @@ const initialTodos: Array<Todo> = [
 const App: React.FunctionComponent = () => {
   const [todos, setTodos] = useState(initialTodos);
   //토글 함수
-  //newTodos에 선택된 todo의 completed를 반대로 바꿔서 담음
+  //newTodos에 선택된(e=selectedTodo) todo의 completed를 반대로 바꿔서 담음
   //newTodos로 setTodos
   const toggleTodo: ToggleTodo = (selectedTodo) => {
     const newTodos = todos.map((todo) => {
@@ -23,9 +24,17 @@ const App: React.FunctionComponent = () => {
     });
     setTodos(newTodos);
   };
+
+  //투두 추가 함수(props로 AddTodoForm에 내려주면 자식업데이트시 부모에서도 업데이트됨)
+  const addTodo: AddTodo = (newTodo) => {
+    newTodo.trimEnd() !== "" &&
+      setTodos([...todos, { text: newTodo, complete: false }]);
+  };
+
   return (
     <React.Fragment>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
     </React.Fragment>
   );
 };
